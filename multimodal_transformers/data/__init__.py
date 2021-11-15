26126aaeaa5666482c35f6c18368b5a594060ac3
jianzheng.nie <jianzhnie@126.com>
2021-11-15 18:39:57 +0800

    update multimodal_transformers

--- multimodal_transformers/data/__init__.py
+++ multimodal_transformers/data/__init__.py
@@ -1,8 +1,12 @@
 '''
 Author: jianzhnie
 Date: 2021-11-08 18:25:03
-LastEditTime: 2021-11-08 18:29:39
+LastEditTime: 2021-11-15 18:37:25
 LastEditors: jianzhnie
 Description:
 
 '''
+
+from .preprocessor.tab_preprocessor import TabPreprocessor
+
+__all__ = ['TabPreprocessor']
--- multimodal_transformers/models/text/__init__.py
+++ multimodal_transformers/models/text/__init__.py
@@ -1,8 +1,14 @@
 '''
 Author: jianzhnie
 Date: 2021-11-08 17:11:49
-LastEditTime: 2021-11-09 13:10:09
+LastEditTime: 2021-11-15 18:38:59
 LastEditors: jianzhnie
 Description:
 
 '''
+
+from .deeptext import BertWithTabular
+from .deeptext_auto import AutoModelWithText
+
+
+__all__ = ['BertWithTabular', 'AutoModelWithText']
