a33d0821d3fd1aeaadb6c91e1a2c6f4a711d6204
jianzheng.nie <jianzhnie@126.com>
2021-11-16 14:07:13 +0800

    update multimodal transformers

--- multimodal_transformers/data/multidomal_dataset.py
+++ multimodal_transformers/data/multidomal_dataset.py
@@ -1,7 +1,7 @@
 '''
 Author: jianzhnie
 Date: 2021-11-12 14:42:32
-LastEditTime: 2021-11-12 14:42:53
+LastEditTime: 2021-11-16 12:11:17
 LastEditors: jianzhnie
 Description:
 
@@ -108,3 +108,60 @@ class WideDeepDataset(Dataset):
             return len(self.X_text)
         if self.X_img is not None:
             return len(self.X_img)
+
+
+class TorchTabularTextDataset(Dataset):
+    """
+    :obj:`TorchDataset` wrapper for text dataset with categorical features
+    and numerical features
+
+    Parameters:
+        encodings (:class:`transformers.BatchEncoding`):
+            The output from encode_plus() and batch_encode() methods (tokens, attention_masks, etc) of
+            a transformers.PreTrainedTokenizer
+        categorical_feats (:class:`numpy.ndarray`, of shape :obj:`(n_examples, categorical feat dim)`, `optional`, defaults to :obj:`None`):
+            An array containing the preprocessed categorical features
+        numerical_feats (:class:`numpy.ndarray`, of shape :obj:`(n_examples, numerical feat dim)`, `optional`, defaults to :obj:`None`):
+            An array containing the preprocessed numerical features
+        labels (:class: list` or `numpy.ndarray`, `optional`, defaults to :obj:`None`):
+            The labels of the training examples
+        class_weights (:class:`numpy.ndarray`, of shape (n_classes),  `optional`, defaults to :obj:`None`):
+            Class weights used for cross entropy loss for classification
+        df (:class:`pandas.DataFrame`, `optional`, defaults to :obj:`None`):
+            Model configuration class with all the parameters of the model.
+            This object must also have a tabular_config member variable that is a
+            TabularConfig instance specifying the configs for TabularFeatCombiner
+
+    """
+
+    def __init__(self,
+                 text_encodings,
+                 tab_feats,
+                 labels=None,
+                 label_list=None,
+                 class_weights=None):
+        self.encodings = text_encodings
+        self.tab_feats = tab_feats
+        self.labels = labels
+        self.class_weights = class_weights
+        self.label_list = label_list if label_list is not None else [
+            i for i in range(len(np.unique(labels)))
+        ]
+
+    def __getitem__(self, idx):
+        item = {
+            key: torch.tensor(val[idx])
+            for key, val in self.encodings.items()
+        }
+        item['labels'] = torch.tensor(
+            self.labels[idx]) if self.labels is not None else None
+        item['cat_feats'] = torch.tensor(self.tab_feats[idx]).float() \
+            if self.tab_feats is not None else torch.zeros(0)
+        return item
+
+    def __len__(self):
+        return len(self.labels)
+
+    def get_labels(self):
+        """returns the label names for classification."""
+        return self.label_list
