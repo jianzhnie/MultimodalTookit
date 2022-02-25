80f738ed6139d231f59b1cca94f950f26d0402f7
jianzheng.nie <jianzhnie@126.com>
2022-02-25 11:41:29 +0800

    add mmt

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,14 +1,14 @@
 ###
  # @Author: jianzhnie
  # @Date: 2021-11-16 14:14:32
- # @LastEditTime: 2021-12-03 15:49:52
+ # @LastEditTime: 2022-02-24 16:13:51
  # @LastEditors: jianzhnie
  # @Description:
  #
 ###
 
 python main.py \
-    --output_dir=./logs/test \
+    --output_dir=./logs/Womens_Clothing_E-Commerce_Reviews \
     --task=classification \
     --combine_feat_method=individual_mlps_on_cat_and_numerical_feats_then_concat \
     --do_train \
